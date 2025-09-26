a08c0a905949e5576cf076741aeab23a761ca16c
asoutrelle <asoutrelleperez@alumnos.exa.unicen.edu.ar>
2025-09-26 19:52:21 -0300

    Nueva clase Colores: permite imprimir por pantalla caracteres con colores

---
+++ Colores.java
@@ -0,0 +1,8 @@
+public class Colores {
+    // Códigos ANSI
+    public static final String RESET = "\u001B[0m";
+    public static final String ROJO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARILLO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";
+}
