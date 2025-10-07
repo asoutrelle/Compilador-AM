e1b43f27240f125d814eac61a3726b4eb101fb40
asoutrelle <asoutrelleperez@alumnos.exa.unicen.edu.ar>
2025-10-07 04:31:39 -0300

    add imprimir reglas gramaticales detectadas

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList<String> estructurasDetectadas = new ArrayList<>();
     public static void main(String[] args) throws IOException {
 
         /*if (args.length < 1) {
@@ -19,6 +22,10 @@ public class Main {
         Parser parser = new Parser(true);
         parser.yyparse();
         lexer.print();
+        System.out.println(Colores.AZUL+"------------------ ESTRUCTURAS DETECTADAS ------------------"+Colores.RESET);
+        for(String str : estructurasDetectadas){
+            System.out.println(str);
+        }
     }
 
 }
