ba51b5b190a71b0e2fb1c6a716cc921a0c4c1806
asoutrelle <asoutrelleperez@alumnos.exa.unicen.edu.ar>
2025-11-13 20:27:02 -0300

    add Terceto.java

--- Colores.java
+++ Colores.java
@@ -5,4 +5,5 @@ public class Colores {
     public static final String VERDE = "\u001B[32m";
     public static final String AMARILLO = "\u001B[33m";
     public static final String AZUL = "\u001B[34m";
+    public static final String MAGENTA = "\u001B[35m";
 }
---
+++ Terceto.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Terceto {
+    private String operacion;
+    private String variable;
+    private String valor;
+
+    public Terceto(String operacion, String variable, String valor) {
+        this.operacion = operacion;
+        this.variable = variable;
+        this.valor = valor;
+    }
+
+    public void print(){
+        System.out.println(Colores.MAGENTA+"(" + operacion + ", " + variable + ", " + valor + ")"+Colores.RESET);
+    }
+}
